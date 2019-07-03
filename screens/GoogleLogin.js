8c492ad23d68dac1c3685a22f1614d8231e1e9d5
timmy1420 <tim.poco01@gmail.com>
2019-07-03 10:06:26 -0300

    Google login now working. But doesnt return access token

--- screens/GoogleLogin.js
+++ screens/GoogleLogin.js
@@ -7,11 +7,11 @@ import {
   Text,
   Button
 } from 'react-native';
-import Expo from 'expo';
+import { Google } from 'expo';
 
 class GoogleLogin extends Component {
   state = {
-    androidClientId: '18078930342-vjkpla7k4mlainq0i5sjjvlaf7rbbijc.apps.googleusercontent.com',
+    androidClientId: 'AIzaSyAgtZrDF94JOXWmbRrj4w5sHON_NY8FjB8',
     iosClientId: null
   };
 
@@ -19,25 +19,36 @@ class GoogleLogin extends Component {
     // this.signInWithGoogleAsync();
   }
 
-  signInWithGoogleAsync = async () => {
-    try {
-      const result = await Expo.Google.logInAsync({
-        androidClientId: '18078930342-vjkpla7k4mlainq0i5sjjvlaf7rbbijc.apps.googleusercontent.com',
-        // iosClientId: this.state.iosClientId,
-        scopes: ['profile', 'email'],
-      });
+  // signInWithGoogleAsync = async () => {
+  //   try {
+  //     const result = await Expo.Google.logInAsync({
+  //       androidClientId: '18078930342-vjkpla7k4mlainq0i5sjjvlaf7rbbijc.apps.googleusercontent.com',
+  //       // iosClientId: this.state.iosClientId,
+  //       scopes: ['profile', 'email'],
+  //     });
   
-      if (result.type === 'success') {
-        console.log('AccessToken', result.accessToken);
-        return result.accessToken;
-      } else {
-        console.log('Login cancelled.');
-        // return { cancelled: true };
-      }
-    } catch (e) {
-      console.log('Google Error', e);
-      // return { error: true };
+  //     if (result.type === 'success') {
+  //       console.log('AccessToken', result.accessToken);
+  //       return result.accessToken;
+  //     } else {
+  //       console.log('Login cancelled.');
+  //       // return { cancelled: true };
+  //     }
+  //   } catch (e) {
+  //     console.log('Google Error', e);
+  //     // return { error: true };
+  //   }
+  // }
+
+  signIn = async () => {
+    const clientId = '18078930342-vjkpla7k4mlainq0i5sjjvlaf7rbbijc.apps.googleusercontent.com';
+    const { type, accessToken, user } = await Google.logInAsync({ clientId });
+
+    if (type === 'success') {
+      /* `accessToken` is now valid and can be used to get data from the Google API with HTTP requests */
+      console.log(user);
     }
+
   }
 
   render() {
@@ -46,7 +57,7 @@ class GoogleLogin extends Component {
     return ( 
       <View style = { styles.container } >
         <Text>Google login page</Text>
-        <Button onPress={this.signInWithGoogleAsync}
+        <Button onPress={this.signIn}
           title="Login"
           color="blue"
           accessibilityLabel="Learn more about this purple button"
